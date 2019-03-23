4b757cf546a33e8f1c379bb1a012203d4fd3c035
junlee91 <jlee757@uic.edu>
2019-03-23 18:56:25 -0500

    sendMail Function with Nodemailer

--- src/server.js
+++ src/server.js
@@ -3,10 +3,9 @@ import path from 'path';
 import { GraphQLServer } from 'graphql-yoga';
 import logger from 'morgan';
 import schema from './schema';
+import { sendSecretMail } from './utils';
 
 dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-require('dotenv').config();
 
 const PORT = process.env.PORT || 4000;
 
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,35 @@
+import nodemailer from 'nodemailer';
+import sgTransport from 'nodemailer-sendgrid-transport';
+import dotenv from 'dotenv';
+import path from 'path';
 import { adjectives, nouns } from './words';
+
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+const sendMail = email => {
+  const options = {
+    auth: {
+      api_user: process.env.SENDGRID_USERNAME,
+      api_key: process.env.SENDGRID_PASSWORD
+    }
+  };
+  const client = nodemailer.createTransport(sgTransport(options));
+
+  return client.sendMail(email);
+};
+
+export const sendSecretMail = (address, secret) => {
+  const email = {
+    from: 'jun@prismagram.com',
+    to: address,
+    subject: '🔒Login Secret for Prismagram 🔒',
+    html: `Hello! Your login secret is ${secret}.<br />Copy paste on the app/website to login`
+  };
+
+  return sendMail(email);
 };
