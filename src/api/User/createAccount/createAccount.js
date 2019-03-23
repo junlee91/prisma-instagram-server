3464d3feabd8aeab54834179ad623822539c8f80
junlee91 <jlee757@uic.edu>
2019-03-23 17:06:56 -0500

    Create Account Resolver

---
+++ src/api/User/createAccount/createAccount.js
@@ -0,0 +1,12 @@
+import { prisma } from '../../../../generated/prisma-client';
+
+export default {
+  Mutation: {
+    createAccount: async (_, args) => {
+      const { username, email, firstName = '', lastName = '', bio = '' } = args;
+      const user = await prisma.createUser({ username, email, firstName, lastName, bio });
+
+      return user;
+    }
+  }
+};
