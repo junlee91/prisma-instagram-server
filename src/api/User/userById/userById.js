84f6c5c75d7b973398c27101d0b96c491ef8c8e6
junlee91 <jlee757@uic.edu>
2019-03-23 15:36:52 -0500

    Resolvers with Prisma

--- src/api/User/userById/userById.js
+++ src/api/User/userById/userById.js
@@ -2,9 +2,9 @@ import { prisma } from '../../../../generated/prisma-client';
 
 export default {
   Query: {
-    sayHello: async () => {
-      console.log(await prisma.users());
-      return 'HELLO';
+    userById: async (_, args) => {
+      const { id } = args;
+      return prisma.user({ id });
     }
   }
 };
