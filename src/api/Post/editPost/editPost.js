64cc09730a8f83a511b77658ef1d465752e3dffd
junlee91 <jlee757@uic.edu>
2019-04-05 21:43:53 -0500

    Edit post resolver

---
+++ src/api/Post/editPost/editPost.js
@@ -0,0 +1,23 @@
+import { prisma } from '../../../../generated/prisma-client';
+
+export default {
+  Mutation: {
+    editPost: async (_, args, { request, isAuthenticated }) => {
+      isAuthenticated(request);
+      const { id, caption, location, action } = args;
+      const { user } = request;
+      const post = await prisma.$exists.post({ id, user: { id: user.id } });
+
+      if (post) {
+        if (action === 'EDIT') {
+          return prisma.updatePost({ data: { caption, location }, where: { id } });
+        }
+        if (action === 'DELETE') {
+          return prisma.deletePost({ where: { id } });
+        }
+      }
+
+      throw Error("You can't do that");
+    }
+  }
+};
