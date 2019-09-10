0d491160c65e91edd1116cba0e06d9e12159dcba
junlee91 <jlee757@uic.edu>
2019-09-09 21:02:41 -0500

    Uploading to Backend

--- src/upload.js
+++ src/upload.js
@@ -1,10 +1,10 @@
 import multer from 'multer';
 
 const upload = multer({ dest: 'uploads/' });
+
 export const uploadMiddleware = upload.single('file');
 
 export const uploadController = (req, res) => {
-  const { file } = req;
-  console.log(file);
-  res.end();
+  const { file: { path } } = req;
+  res.json({ path });
 };
